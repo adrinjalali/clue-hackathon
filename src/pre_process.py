2b1a1f294f29f08f348d6b1dbdff4777d502317c
adrin <adrin.jalali@gmail.com>
2017-03-14 22:14:55 +0100

    get transformed day_in_cycle values

--- src/pre_process.py
+++ src/pre_process.py
@@ -1,6 +1,9 @@
 from os.path import join
 import os
 import pandas as pd
+from pandasql import sqldf
+from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
 
 def save_binary(data_dir,
                 active_days = 'active_days.csv',
@@ -38,26 +41,42 @@ def load_binary():
             'tracking': df_tracking}
     
 
-def process_level1(tracking):
+def process_level1(tracking, cycles):
     """
-    pivot table and put symptoms on columns to get a matrix.
+    Pivot table and put symptoms on columns to get a matrix.
+    Then convert the day_in_cycle into:
+    - proportionate value to the cycle
+    - distance from the end of the cycle
+    - distance from the end of the period
+    These values can be usefull later to map symptoms of 
+    different cycles together. It might also help mapping
+    different cycles of different people together.
     """
-    return tracking[['user_id', 'cycle_id', 'day_in_cycle', 'date', 'symptom']]\
-        .pivot_table(index= ['user_id', 'cycle_id', 'day_in_cycle', 'date'],
-                     columns = ['symptom'], aggfunc=lambda x: 1).\
-                     reset_index()
+    val = tracking[['user_id', 'cycle_id', 'day_in_cycle', 'date', 'symptom']]\
+          .pivot_table(index= ['user_id', 'cycle_id', 'day_in_cycle', 'date'],
+                       columns = ['symptom'], aggfunc=lambda x: 1).\
+                       reset_index()
+    
+    val = sqldf("""select t1.*,
+                   c1.cycle_length, c1.expected_cycle_length, c1.period_length,
+                   t1.day_in_cycle / c1.cycle_length as proportionate,
+                   c1.cycle_length - t1.day_in_cycle as inverse,
+                   t1.day_in_cycle - c1.period_length as period_removed
+                   from val as t1 join cycles as c1 on t1.user_id = c1.user_id 
+                   and t1.cycle_id = c1.cycle_id""",
+                {'val': val, 'cycles': cycles})
+    return val
 
 def process_level2(l1_tracking, active_days):
     """
     WIP
     """
+    pysqldf = lambda q: sqldf(q, globals())
 
     data = load_binary()
     users, cycles, active_days, tracking = data['users'], data['cycles'], data['active_days'], data['tracking']
-    l1_tracking = process_level1(tracking)
+    l1_tracking = process_level1(tracking, cycles)
 
-    from pandasql import sqldf
-    pysqldf = lambda q: sqldf(q, globals())
 
     for user in users.user_id:
         a = l1_tracking[l1_tracking.user_id == user]
@@ -67,9 +86,8 @@ def process_level2(l1_tracking, active_days):
             break
 
         
-    from scipy.interpolate import interp1d
-    from scipy.signal import savgol_filter
-    y = a.unprotected_sex.values
+    symptom = 'unprotected_sex'
+    y = a[symptom].values
     y[np.isnan(y)] = 0
     x = np.array(list(range(len(a))))
     xx = np.linspace(x.min(),x.max(), len(x))
