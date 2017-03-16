7341ca75bd7835b18662e647bf58108d6896fe67
JB <braun.johannes@gmail.com>
2017-03-16 17:52:40 +0100

    added process_explode methode that adds missing dates to all cycles

--- src/pre_process.py
+++ src/pre_process.py
@@ -4,6 +4,8 @@ import pandas as pd
 from pandasql import sqldf
 from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
+from pandas import DataFrame
+from tqdm import tqdm
 
 def save_binary(data_dir,
                 active_days = 'active_days.csv',
@@ -66,6 +68,117 @@ def process_level1(tracking, cycles):
                    and t1.cycle_id = c1.cycle_id""",
                 {'val': val, 'cycles': cycles})
     return val
+
+
+
+def process_explode(tracking, cycles):
+    """
+    Pivot table and put symptoms on columns to get a matrix.
+
+    Then explode missing dates
+
+    Then convert the day_in_cycle into:
+    - proportionate value to the cycle
+    - distance from the end of the cycle
+    - distance from the end of the period
+    These values can be usefull later to map symptoms of
+    different cycles together. It might also help mapping
+    different cycles of different people together.
+    """
+
+    tracking_pivot = tracking[['user_id', 'cycle_id', 'day_in_cycle', 'date', 'symptom']] \
+        .pivot_table(index=['user_id', 'cycle_id', 'day_in_cycle', 'date'],
+                     columns=['symptom'], aggfunc=lambda x: 1). \
+        reset_index()
+
+    tracking_pivot["date"] = pd.to_datetime(tracking_pivot["date"])
+    cycles["cycle_start"] = pd.to_datetime(cycles["cycle_start"])
+
+    tracking_pivot = tracking_pivot.sort_values(by=["user_id", "date"], ascending=[False, True])
+    tracking_pivot.reset_index(drop=True)
+
+    full_cycles = []
+
+    for i, cycle in tqdm(cycles.iterrows()):
+        cycle_start = cycle["cycle_start"]
+        cycle_length = cycle["cycle_length"]
+        cycle_end = cycle_start + pd.DateOffset(cycle_length - 1)
+        idx = pd.date_range(cycle_start, cycle_end, freq="D")
+        full_df = DataFrame(idx, columns=["date"])
+
+        full_df["user_id"] = cycle["user_id"]
+        full_df["cycle_id"] = cycle["cycle_id"]
+        full_df["period_length"] = cycle["period_length"]
+        full_df["cycle_length"] = cycle_length
+        full_df["expected_cycle_length"] = cycle["expected_cycle_length"]
+        full_df["day_in_cycle"] = range(1, int(cycle_length) + 1, 1)
+        full_df["proportionate"] = full_df["day_in_cycle"] / cycle_length
+        full_df["inverse"] = cycle_length - full_df["day_in_cycle"]
+        full_df["period_removed"] = full_df["day_in_cycle"] - cycle["period_length"]
+
+        full_cycles.append(full_df)
+
+    full_cycles = pd.concat(full_cycles)
+    full_tracking_pivot = pd.merge(full_cycles, tracking_pivot, how='left',
+                                   on=['user_id', 'cycle_id', "day_in_cycle", "date"])
+
+    return full_tracking_pivot
+
+
+
+def process_explode(tracking, cycles):
+    """
+    Pivot table and put symptoms on columns to get a matrix.
+
+    Then explode missing dates
+
+    Then convert the day_in_cycle into:
+    - proportionate value to the cycle
+    - distance from the end of the cycle
+    - distance from the end of the period
+    These values can be usefull later to map symptoms of
+    different cycles together. It might also help mapping
+    different cycles of different people together.
+    """
+
+    tracking_pivot = tracking[['user_id', 'cycle_id', 'day_in_cycle', 'date', 'symptom']] \
+        .pivot_table(index=['user_id', 'cycle_id', 'day_in_cycle', 'date'],
+                     columns=['symptom'], aggfunc=lambda x: 1). \
+        reset_index()
+
+    tracking_pivot["date"] = pd.to_datetime(tracking_pivot["date"])
+    cycles["cycle_start"] = pd.to_datetime(cycles["cycle_start"])
+
+    tracking_pivot = tracking_pivot.sort_values(by=["user_id", "date"], ascending=[False, True])
+    tracking_pivot.reset_index(drop=True)
+
+    full_cycles = []
+
+    for i, cycle in tqdm(cycles.iterrows()):
+        cycle_start = cycle["cycle_start"]
+        cycle_length = cycle["cycle_length"]
+        cycle_end = cycle_start + pd.DateOffset(cycle_length - 1)
+        idx = pd.date_range(cycle_start, cycle_end, freq="D")
+        full_df = DataFrame(idx, columns=["date"])
+
+        full_df["user_id"] = cycle["user_id"]
+        full_df["cycle_id"] = cycle["cycle_id"]
+        full_df["period_length"] = cycle["period_length"]
+        full_df["cycle_length"] = cycle_length
+        full_df["expected_cycle_length"] = cycle["expected_cycle_length"]
+        full_df["day_in_cycle"] = range(1, int(cycle_length) + 1, 1)
+        full_df["proportionate"] = full_df["day_in_cycle"] / cycle_length
+        full_df["inverse"] = cycle_length - full_df["day_in_cycle"]
+        full_df["period_removed"] = full_df["day_in_cycle"] - cycle["period_length"]
+
+        full_cycles.append(full_df)
+
+    full_cycles = pd.concat(full_cycles)
+    full_tracking_pivot = pd.merge(full_cycles, tracking_pivot, how='left',
+                                   on=['user_id', 'cycle_id', "day_in_cycle", "date"])
+
+    return full_tracking_pivot
+
 
 def process_level2(l1_tracking, active_days):
     """
