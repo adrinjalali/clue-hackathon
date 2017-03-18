36c18e126e34ad187710046e69a084da0d4ab606
adrin <adrin.jalali@gmail.com>
2017-03-18 14:36:44 +0100

    preprocessing cleanup

--- src/pre_process.py
+++ src/pre_process.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 import numpy as np
 
 def save_binary(data_dir,
+                binary_dir = 'binary',
                 active_days = 'active_days.csv',
                 cycles = 'cycles.csv',
                 tracking = 'tracking.csv',
@@ -22,21 +23,21 @@ def save_binary(data_dir,
     df_cycles = pd.read_csv(join(data_dir, cycles))
     df_users = pd.read_csv(join(data_dir, users))
     df_tracking = pd.read_csv(join(data_dir, tracking))
-    os.makedirs('../binary', exist_ok=True)
-    df_active_days.to_pickle('../binary/active_days.pkl')
-    df_cycles.to_pickle('../binary/cycles.pkl')
-    df_users.to_pickle('../binary/users.pkl')
-    df_tracking.to_pickle('../binary/tracking.pkl')
+    os.makedirs(binary_dir, exist_ok=True)
+    df_active_days.to_pickle(os.path.join(binary_dir, 'active_days.pkl'))
+    df_cycles.to_pickle(os.path.join(binary_dir, 'cycles.pkl'))
+    df_users.to_pickle(os.path.join(binary_dir, 'users.pkl'))
+    df_tracking.to_pickle(os.path.join(binary_dir, 'tracking.pkl'))
 
 
-def load_binary():
+def load_binary(binary_dir = 'binary'):
     """
     loads the binary data.
     """
-    df_active_days = pd.read_pickle('../binary/active_days.pkl')
-    df_cycles = pd.read_pickle('../binary/cycles.pkl')
-    df_users = pd.read_pickle('../binary/users.pkl')
-    df_tracking = pd.read_pickle('../binary/tracking.pkl')
+    df_active_days = pd.read_pickle(os.path.join(binary_dir, 'active_days.pkl'))
+    df_cycles = pd.read_pickle(os.path.join(binary_dir, 'cycles.pkl'))
+    df_users = pd.read_pickle(os.path.join(binary_dir, 'users.pkl'))
+    df_tracking = pd.read_pickle(os.path.join(binary_dir, 'tracking.pkl'))
 
     return {'users': df_users,
             'cycles': df_cycles,
@@ -132,36 +133,31 @@ def process_explode(tracking, cycles):
     return full_tracking_pivot
 
 
-def process_level2(l1_tracking, active_days):
-    """
-    WIP
-    """
-    pysqldf = lambda q: sqldf(q, globals())
+def process_level2(data):
+    val = sqldf("""select t1.*,
+                   c1.cycle_length, c1.expected_cycle_length, c1.period_length,
+                   (t1.day_in_cycle / c1.cycle_length) * 29 as proportionate,
+                   c1.cycle_length - t1.day_in_cycle as inverse,
+                   ((c1.cycle_length - t1.day_in_cycle) / c1.cycle_length) * 29 as inverse_proportionate,
+                   t1.day_in_cycle - c1.period_length as period_removed
+                   from val as t1 join cycles as c1 on t1.user_id = c1.user_id
+                   and t1.cycle_id = c1.cycle_id""",
+                {'val': data['tracking'], 'cycles': data['cycles']})
+    day_in_cycle = pandas.DataFrame(np.array(range(29)), columns = ['day'])
 
-    data = load_binary()
-    users, cycles, active_days, tracking = data['users'], data['cycles'], data['active_days'], data['tracking']
-    l1_tracking = process_level1(tracking, cycles)
+    val.proportionate = val.proportionate.astype(int)
+    val.inverse_proportionate = val.inverse_proportionate.astype(int)
+
+    v2 = sqldf("""select t1.user_id, t1.category, t1.symptom, t1.inverse as X, count(*) as cnt
+                  from tbl as t1
+                  group by user_id, category, symptom, inverse
+                  order by inverse""",
+               {'tbl': val})
+
+    v3 = v2.pivot_table(index= ['user_id'],
+                        columns = ['X', 'category', 'symptom'], aggfunc=lambda x: sum(x)).\
+                        reset_index()
+    return v3
 
 
-    for user in users.user_id:
-        a = l1_tracking[l1_tracking.user_id == user]
-        b = active_days[active_days.user_id == user]
-        if len(a) != len(b):
-            print(user)
-            break
-
-
-    symptom = 'unprotected_sex'
-    y = a[symptom].values
-    y[np.isnan(y)] = 0
-    x = np.array(list(range(len(a))))
-    xx = np.linspace(x.min(),x.max(), len(x))
-    itp = interp1d(x,y, kind='linear')
-    window_size, poly_order = 5, 3
-    yy_sg = savgol_filter(itp(xx), window_size, poly_order)
-    print(np.vstack((yy_sg, y)).transpose())
-
-    """
-    map things to [0,1], by connecting cycles to tracking,
-    group by day in cycle, take max or sum.
-    """
+    
