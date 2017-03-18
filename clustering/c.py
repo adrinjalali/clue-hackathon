797d08af62f82f54a1df4dadbd3025b3a0de6ab4
Gianluca Segato <gianluca.segato@gmail.com>
2017-03-18 16:46:53 +0100

    Fixed continent mapping

--- clustering/c.py
+++ clustering/c.py
@@ -8,21 +8,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
 # preparing countries to continent mapping
-reader = csv.reader(open('countries_mapping_c.csv', 'r'))
-d_continent = {}
+reader = csv.reader(open('countries_mapping.csv', 'r'))
+d = {}
 for row in reader:
-    d_continent[row[1]] = row[0]
-
-reader = csv.reader(open('countries_mapping_r.csv', 'r'))
-d_region = {}
-for row in reader:
-    d_region[row[0]] = row[6]
+    d[row[1]] = row[0]
 
 # preparing data
-df_u = pd.read_csv("users.csv")
-df_u = df_u.apply(lambda x: x.fillna(x.median()) if np.issubdtype(x.dtype, np.number) else x, axis=0)
-df_u['continent'] = df_u.country.map(d)
-df_u = df_u[(pd.notnull(df_u['continent']))] #df_u[(pd.isnull(df_u['continent']))]
+df_u = pd.read_csv("users.csv") \
+    .apply(lambda x: x.fillna(x.median()) if np.issubdtype(x.dtype, np.number) else x, axis=0)
+df_u['continent'] = df_u.country.map(d).fillna("Oceania")
 
 # preparing the dataset to cluster
 drop_elements = ['Oceania', 'user_id', 'country', 'platform', 'continent']
@@ -38,11 +32,10 @@ train['height'] = scaler.fit_transform(train['birthyear'].reshape(-1, 1))
 H_clusters = 8
 df_u['cluster'] = KMeans(n_clusters = H_clusters).fit_predict(train.values)
 
-Z = linkage(train.values, 'ward')
+# Z = linkage(train.values, 'ward')
 
 analysis = pd.concat([df_u[['birthyear', 'weight', 'height']].groupby(df_u.cluster).mean(),
-    df_u.continent.groupby(df_u.cluster).agg(lambda x:x.value_counts().index[0]),
-    df_u.continent_label.groupby(df_u.cluster).mean()], axis = 1)
+    df_u.continent.groupby(df_u.cluster).agg(lambda x:x.value_counts().index[0])], axis = 1)
 
 df_u.cluster.groupby(df_u.continent).mean()
 
